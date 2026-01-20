fb867b7a52dd3295f64de72df0558dd4dd35197b
Mostafa Edrees <mostafaedrees018@gmail.com>
2026-01-20 11:00:52 +0200

    feat: implement the UI of Navbar & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
